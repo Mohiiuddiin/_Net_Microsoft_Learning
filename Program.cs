d55f9231d0db460d79ab92e68b9a0cfea304c1e9
Mohiiuddiin <md.mohiiuddiin@gmail.com>
2024-05-27 09:54:34 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.IO;
+
+namespace _Net_Microsoft_Learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
